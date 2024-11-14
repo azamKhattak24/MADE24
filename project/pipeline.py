4b0a77e3f8718c20d9ce0b0325a1b26766574a91
Azam Khattak <azamkhtk@gmail.com>
2024-11-14 08:23:06 +0100

    Kaggle Credential Removed

--- project/pipeline.py
+++ project/pipeline.py
@@ -259,8 +259,8 @@ def main():
         - Save the transformed data to a SQLite database.
     """
 #   Define Kaggle credentials to be found in kaggle.JSON
-    username = "azamkhattak"
-    key = "3932ac8e3dca141cad0d6841bd395cc8"
+    username = "username"
+    key = "key"
 #     kaggle_extract_Data(username, key)
     deforestration_df = kaggle_extract_Data(username, key)
     
--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Define Kaggle credentials to be found in kaggle.JSON on line 262
-python3 pipeline.py
+# Define Kaggle credentials in pipeline.py to be found in kaggle.JSON on line 262
+python3 project/pipeline.py
