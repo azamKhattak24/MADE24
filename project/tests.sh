82c35ce14378ff59bcbd939dd5a4249707f42105
Azam Khattak <azamkhtk@gmail.com>
2024-12-12 21:57:06 +0100

    test updated

--- project/tests.sh
+++ project/tests.sh
@@ -5,6 +5,5 @@
 # echo ">>> installing required packages..."
 # pip3  install -r ./project/requirements.txt
 echo ">>> running the tests ..."
-cd ./project
 pytest automated_test.py -v ;
 echo ">>> tests ran..."
