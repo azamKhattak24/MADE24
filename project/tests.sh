45253716f656d155e759847dee6137abbb6599b1
Azam Khattak <azamkhtk@gmail.com>
2024-12-12 12:50:58 +0100

    CI updated

--- project/tests.sh
+++ project/tests.sh
@@ -2,8 +2,8 @@
 #To execute the '.sh' file on your local system, ensure you modify the file paths accordingly to match your local directory structure.
 #pip install -r requirements.txt
 #if python3 automated_testing.py ;
-echo ">>> installing required packages..."
-pip3  install -r ./project/requirements.txt
+# echo ">>> installing required packages..."
+# pip3  install -r ./project/requirements.txt
 echo ">>> running the tests ..."
 if python3 ./project/automated_test.py ; then 
 echo ">>> tests ran..."
